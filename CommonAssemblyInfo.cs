0fe6ac575da0281c6bc0e6a3b07dc05b3ca3188d
Yaakov Smith <yaakov.smith@wisetechglobal.com>
2017-03-29 10:12:29 +0000

    WTG.A WI00155765 NuGet 3 - Package WTG.Analyzers as a NuGet package

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -3,9 +3,10 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("WiseTech Global Pty Ltd")]
 [assembly: AssemblyProduct("WTG Analyzers")]
-[assembly: AssemblyCopyright("Copyright © WiseTech Global 2016-2017")]
+[assembly: AssemblyCopyright("Copyright Â© WiseTech Global 2016-2017")]
 
 // The major and minor versions should match the roslyn api version,
+// and the major/minor/build versions should match the nuget package version (nuget is x.y.z only)
 // Revision should be incremented whenever an analyzer changes in a way that might affect previously cached results.
 [assembly: AssemblyVersion("1.2.0.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
---
+++ WTG.Analyzers.Test/VersioningTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace WTG.Analyzers.Test
+{
+	class VersioningTest
+	{
+		[Test]
+		public void AssemblyMajorMinorVersionMatchesRoslynMajorMinorVersion()
+		{
+			var analyzersVersion = GetAssemblyVersion("WTG.Analyzers");
+			var roslynVersion = GetAssemblyVersion("Microsoft.CodeAnalysis");
+
+			Assert.Multiple(() =>
+			{
+				Assert.That(analyzersVersion.Major, Is.EqualTo(roslynVersion.Major), nameof(Version.Major));
+				Assert.That(analyzersVersion.Minor, Is.EqualTo(roslynVersion.Minor), nameof(Version.Minor));
+			});
+		}
+
+		[Test]
+		public void AssemblyVersionMatchesNuGetPackageVersion()
+		{
+			var packageDirectory = Path.Combine(TestContext.CurrentContext.TestDirectory, "NuGet");
+			var packages = Directory.GetFiles(packageDirectory, "*.nupkg", SearchOption.TopDirectoryOnly);
+
+			Assert.That(packages.Length, Is.EqualTo(1), "Sanity check - Exactly one NuGet package should have been created.");
+
+			var package = packages[0];
+
+			var analyzersVersion = GetAssemblyVersion("WTG.Analyzers");
+			var expectedPackageName = FormattableString.Invariant($"WTG.Analyzers.{analyzersVersion.ToString(fieldCount: 3)}.nupkg");
+			Assert.That(Path.GetFileName(package), Is.EqualTo(expectedPackageName));
+		}
+
+		static Version GetAssemblyVersion(string assemblyName)
+			=> Assembly.Load(assemblyName).GetName().Version;
+	}
+}
