fbba9da95890fe4ebb579182b94d89fb1a5f3c60
Yaakov Smith <yaakov.smith@wisetechglobal.com>
2017-11-27 10:50:50 +1100

    Remove UTF-8 BOM from packaging script

--- build-packages.cmd
+++ build-packages.cmd
@@ -1,3 +1,3 @@
-﻿@echo off
+@echo off
 packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.nuspec -OutputDirectory Bin
 packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.TestFramework.nuspec -OutputDirectory Bin
