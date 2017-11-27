0ef202b673655896cb2d172b4980abd14cdc1047
Yaakov <yaakov-h@users.noreply.github.com>
2017-11-28 09:27:28 +1100

    Add AppVeyor builds
    * Replace Paket.exe with magic-mode bootstrapper
    * update dependencies
    * Update .gitignore
    * Lock Paket to latest version

--- build-packages.cmd
+++ build-packages.cmd
@@ -1,3 +1,3 @@
-﻿@echo off
-packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.nuspec -OutputDirectory Bin
-packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.TestFramework.nuspec -OutputDirectory Bin
+@echo off
+packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.nuspec -OutputDirectory Bin -Version %1
+packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.TestFramework.nuspec -OutputDirectory Bin -Version %1
