f6904b3e56f7553dec2c5bccd67fc3b17221ea05
Yaakov Smith <yaakov.smith@wisetechglobal.com>
2017-12-13 16:16:43 +1100

    Add test case for ambiguous Array class.

---
+++ WTG.Analyzers.Test/TestData/ArrayAnalyzer/EmptyArrayCreationWithAmbiguities/Source.cs
@@ -0,0 +1,21 @@
+using System;
+
+public static class Foo
+{
+	public static void ArrayCreator()
+	{
+		var o = new object[0];
+		var s = new string[0];
+		var i = new int[0];
+		var ni = new int?[0];
+	}
+
+	public static void GenericArrayCreator<T>()
+	{
+		var t = new T[0];
+	}
+}
+
+public class Array
+{
+}
