5efd161af55b6ea5013c9a8b02cd9fb37eeaa623
Yaakov Smith <yaakov.smith@wisetechglobal.com>
2018-08-06 12:10:08 +1000

    Add test for implicit conversion from nullable value type

--- WTG.Analyzers.Test/TestData/NullComparisonAnalyzer/IgnoresImplicitConversions/Source.cs
+++ WTG.Analyzers.Test/TestData/NullComparisonAnalyzer/IgnoresImplicitConversions/Source.cs
@@ -8,3 +8,14 @@ public struct Bob
 
 	public static implicit operator Bob(byte[] val) => default(Bob);
 }
+
+public struct Wendy
+{
+	public static bool IsNull1(Wendy w) => w == null;
+	public static bool IsNull2(Wendy w) => null == w;
+
+	public static bool operator ==(Wendy a, Wendy b) => true;
+	public static bool operator !=(Wendy a, Wendy b) => false;
+
+	public static implicit operator Wendy(int? i) => default(Wendy);
+}
