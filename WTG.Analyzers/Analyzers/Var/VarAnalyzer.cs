2cf3a5e24912e774892fab65612c9fddf165eb4b
Yaakov Smith <yaakov.smith@wisetechglobal.com>
2018-08-13 15:03:39 +1000

    simplify

--- WTG.Analyzers/Analyzers/Var/VarAnalyzer.cs
+++ WTG.Analyzers/Analyzers/Var/VarAnalyzer.cs
@@ -51,12 +51,7 @@ namespace WTG.Analyzers
 
 			var candidate = Visitor.Instance.Visit(context.Node);
 
-			if (candidate == null)
-			{
-				return;
-			}
-
-			if (candidate.ValueSource.IsKind(FutureSyntaxKinds.DefaultLiteralExpression))
+			if (candidate == null || candidate.ValueSource.IsKind(FutureSyntaxKinds.DefaultLiteralExpression))
 			{
 				return;
 			}
