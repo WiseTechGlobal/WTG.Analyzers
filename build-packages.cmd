ae98c0d4e2eaebba6293bbf349866afaa60c22ca
Yaakov Smith <yaakov.smith@wisetechglobal.com>
2018-08-30 13:04:02 +1000

    Add NuGet package for Utils.
    Resolves #42

--- build-packages.cmd
+++ build-packages.cmd
@@ -1,3 +1,4 @@
 @echo off
 packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.nuspec -OutputDirectory Bin -Version %1
+packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.Utils.nuspec -OutputDirectory Bin -Version %1
 packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.TestFramework.nuspec -OutputDirectory Bin -Version %1
