2cabe175c2b976e8eb5f9f98075b78e52fc75a4c
brian-reichle <brian.reichle@wtg.zone>
2019-01-16 18:10:41 +1100

    Track commit id in nuget packages. (#64)

--- build-packages.cmd
+++ build-packages.cmd
@@ -1,4 +1,4 @@
 @echo off
-packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.nuspec -OutputDirectory Bin -Version %1
-packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.Utils.nuspec -OutputDirectory Bin -Version %1
-packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.TestFramework.nuspec -OutputDirectory Bin -Version %1
+packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.nuspec -OutputDirectory Bin -Version %1 -Properties commitid=%2
+packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.Utils.nuspec -OutputDirectory Bin -Version %1 -Properties commitid=%2
+packages\NuGet.CommandLine\tools\NuGet.exe pack WTG.Analyzers.TestFramework.nuspec -OutputDirectory Bin -Version %1 -Properties commitid=%2
