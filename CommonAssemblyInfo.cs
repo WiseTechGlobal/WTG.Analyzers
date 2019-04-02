3839cf96f559a7a8fd1a63f300018ad60d05c5bb
Yaakov <git@yaakov.online>
2019-04-02 19:14:01 +1100

    Get rid of InternalsVisibleTo entirely

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,6 +1,3 @@
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: InternalsVisibleTo("WTG.Analyzers.Test-net46")]
-[assembly: InternalsVisibleTo("WTG.Analyzers.Test-netcoreapp2.1")]
