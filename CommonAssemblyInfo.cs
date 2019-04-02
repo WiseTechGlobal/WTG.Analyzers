bfab2ab207deba417c9fdf8985a80ea23d5e2525
Yaakov <git@yaakov.online>
2019-04-02 19:07:08 +1100

    if the mountain won't come to Muhammad, then Muhammad must go to the mountain

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,4 +2,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: InternalsVisibleTo("WTG.Analyzers.Test")]
+[assembly: InternalsVisibleTo("WTG.Analyzers.Test-net46")]
+[assembly: InternalsVisibleTo("WTG.Analyzers.Test-netcoreapp2.1")]
