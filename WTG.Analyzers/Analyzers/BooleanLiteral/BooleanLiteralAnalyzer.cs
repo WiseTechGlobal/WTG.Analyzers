7be775e05261473c7c31478a1f7bbd66392dbc5f
Yaakov <git@yaakov.online>
2020-10-01 21:04:37 +1000

    Detect non-generic boolean literals used as unnamed method parameters

--- WTG.Analyzers/Analyzers/BooleanLiteral/BooleanLiteralAnalyzer.cs
+++ WTG.Analyzers/Analyzers/BooleanLiteral/BooleanLiteralAnalyzer.cs
@@ -38,6 +38,61 @@ namespace WTG.Analyzers
 			{
 				return;
 			}
+
+			var expression = (LiteralExpressionSyntax)context.Node;
+			if (!expression.Parent.IsKind(SyntaxKind.Argument))
+			{
+				return;
+			}
+
+			var argument = (ArgumentSyntax)expression.Parent;
+			if (argument.NameColon != null)
+			{
+				return;
+			}
+
+			var methodInvocation = argument.FirstAncestorOrSelf<InvocationExpressionSyntax>();
+			if (methodInvocation is null)
+			{
+				return;
+			}
+
+			var methodSymbol = (IMethodSymbol)context.SemanticModel.GetSymbolInfo(methodInvocation, context.CancellationToken).Symbol;
+			if (methodSymbol is null)
+			{
+				return;
+			}
+
+			var argumentList = (ArgumentListSyntax)argument.Parent;
+			var index = FindArgumentIndex(argumentList, argument);
+			var argumentSymbol = methodSymbol.Parameters[index];
+
+			if (argumentSymbol.OriginalDefinition.Type.TypeKind == TypeKind.TypeParameter)
+			{
+				return;
+			}
+
+			context.ReportDiagnostic(
+				Diagnostic.Create(
+					Rules.UseNamedArgumentsWhenPassingBooleanLiteralsRule,
+					argument.GetLocation()));
+		}
+
+		static int FindArgumentIndex(ArgumentListSyntax haystack, ArgumentSyntax needle)
+		{
+			var i = 0;
+			var enumerator = haystack.Arguments.GetEnumerator();
+			while (enumerator.MoveNext())
+			{
+				if (enumerator.Current == needle)
+				{
+					return i;
+				}
+
+				i++;
+			}
+
+			throw new InvalidOperationException("Failed to find Argument in its parent ArgumentList.");
 		}
 	}
 }
