1397fd73e0cbc6bfa01f80ef5dc0ff60731fb7ab
Yaakov Smith <yaakov.smith@wisetechglobal.com>
2020-10-12 11:24:01 +1100

    WTG1018: Add failing test for params args

---
+++ WTG.Analyzers.Test/TestData/BooleanLiteralAnalyzer/ParamsMethodArgument/Source.cs
@@ -0,0 +1,21 @@
+public static class Bob
+{
+	public static void Method()
+	{
+		Foo(null, true, false);
+		Bar(true, false);
+		Baz(true, false);
+	}
+
+	public static void Foo(params object[] args)
+    {
+	}
+
+	public static void Bar(params bool[] args)
+	{
+	}
+
+	public static void Baz<T>(params T[] args)
+	{
+	}
+}
