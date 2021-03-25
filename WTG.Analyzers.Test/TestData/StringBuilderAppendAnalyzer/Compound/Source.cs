939a1592167c39cb607758a2f9f795c2d4946127
brian-reichle <18721383+brian-reichle@users.noreply.github.com>
2021-03-25 12:26:05 +1100

    Added Compound test.

---
+++ WTG.Analyzers.Test/TestData/StringBuilderAppendAnalyzer/Compound/Source.cs
@@ -0,0 +1,13 @@
+using System.Text;
+
+namespace Magic
+{
+	class Bob
+	{
+		void Method(string foo, int bar)
+		{
+			var builder = new StringBuilder();
+			builder.Append(foo + foo.Substring(bar, 3) + $"{foo}:{bar}");
+		}
+	}
+}
