61366ea5341bb0394fdcedd4bd7d4d7278cc5b45
Yaakov Smith <yaakov.smith@wisetechglobal.com>
2021-04-22 15:42:49 +1000

    Add initial tests

---
+++ WTG.Analyzers.Test/TestData/FileSystemPathsAnalyzer/PathCombineSpecified/Source.cs
@@ -0,0 +1,22 @@
+using System.IO;
+
+class Foo
+{
+	const string Child = "grandchild";
+	const string GrandchildFolderName = "grandchild";
+
+	public static void Method()
+	{
+		DoThingWithFile(Path.Combine("parent", "child\\grandchild"));
+		DoThingWithFile(Path.Combine("parent", @"child\grandchild"));
+		DoThingWithFile(Path.Combine("parent", $"child\\{GrandchildFolderName}"));
+		DoThingWithFile(Path.Combine("parent", "child\\" + GrandchildFolderName"));
+		DoThingWithFile(Path.Combine("parent", @"child\" + GrandchildFolderName"));
+		DoThingWithFile(Path.Combine("parent", $"{Child}\\{GrandchildFolderName}"));
+		DoThingWithFile(Path.Combine("parent", @$"{Child}\{GrandchildFolderName}"));
+	}
+
+	static void DoThingWithFile(string file)
+	{
+	}
+}
