1da3d7bc260e5e5cd838f0bf5dc546061a037ce3
Yaakov Smith <yaakov.smith@wisetechglobal.com>
2021-05-11 12:39:20 +1000

    allow paths that are heavily platform-specific

--- WTG.Analyzers/Analyzers/FileSystemPaths/FileSystemPathsAnalyzer.cs
+++ WTG.Analyzers/Analyzers/FileSystemPaths/FileSystemPathsAnalyzer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
@@ -46,18 +47,37 @@ namespace WTG.Analyzers
 
 			var invocation = (InvocationExpressionSyntax)context.Node;
 
-			if (LooksLikePathCombine(invocation) &&
-				invocation.ArgumentList.Arguments.Any(a => ExpressionLooksSuspicious(a.Expression)) &&
-				IsPathCombine(context.SemanticModel, invocation, context.CancellationToken))
+			if (LooksLikePathCombine(invocation))
 			{
-				foreach (var argument in invocation.ArgumentList.Arguments)
+				var arguments = invocation.ArgumentList.Arguments;
+				for (var i = 0; i < arguments.Count; i++)
 				{
-					if (ExpressionIsSuspicious(context.SemanticModel, argument.Expression, context.CancellationToken))
+					if (!ExpressionLooksSuspicious(arguments[i].Expression, isFirst: i == 0))
 					{
+						continue;
+					}
+
+					if (!IsPathCombine(context.SemanticModel, invocation, context.CancellationToken))
+					{
+						// This isn't actually Path.Combine so finish up the loop.
+						break;
+					}
+
+					for (var j = 0; j < arguments.Count; j++)
+					{
+						var argument = arguments[j];
+						if (!ExpressionIsSuspicious(context.SemanticModel, argument.Expression, isFirst: j == 0, context.CancellationToken))
+						{
+							continue;
+						}
+
 						context.ReportDiagnostic(Diagnostic.Create(
 							Rules.DoNotUsePathSeparatorsInPathLiteralsRule,
 							argument.GetLocation()));
 					}
+
+					// We've examined all arguments against the semantic model now, so finish up the loop.
+					break;
 				}
 			}
 		}
@@ -79,21 +99,21 @@ namespace WTG.Analyzers
 			return syntax.ArgumentList.Arguments.Count >= 2;
 		}
 
-		static bool ExpressionLooksSuspicious(ExpressionSyntax expression)
+		static bool ExpressionLooksSuspicious(ExpressionSyntax expression, bool isFirst)
 		{
 			switch (expression.Kind())
 			{
 				case SyntaxKind.StringLiteralExpression:
 					var literal = (LiteralExpressionSyntax)expression;
-					return StringLiteralExpressionIsSuspicious(literal);
+					return StringLiteralExpressionIsSuspicious(literal, isFirst);
 
 				case SyntaxKind.AddExpression:
 					var add = (BinaryExpressionSyntax)expression;
-					return ExpressionLooksSuspicious(add.Left) || ExpressionLooksSuspicious(add.Right);
+					return ExpressionLooksSuspicious(add.Left, isFirst) || ExpressionLooksSuspicious(add.Right, isFirst: false);
 
 				case SyntaxKind.InterpolatedStringExpression:
 					var interpolatedString = (InterpolatedStringExpressionSyntax)expression;
-					return InterpolatedStringIsSuspicious(interpolatedString);
+					return InterpolatedStringIsSuspicious(interpolatedString, isFirst);
 
 				case SyntaxKind.IdentifierName:
 					return true;
@@ -103,47 +123,49 @@ namespace WTG.Analyzers
 			}
 		}
 
-		static bool ExpressionIsSuspicious(SemanticModel semanticModel, ExpressionSyntax expression, CancellationToken cancellationToken)
+		static bool ExpressionIsSuspicious(SemanticModel semanticModel, ExpressionSyntax expression, bool isFirst, CancellationToken cancellationToken)
 		{
 			switch (expression.Kind())
 			{
 				case SyntaxKind.StringLiteralExpression:
 					var literal = (LiteralExpressionSyntax)expression;
-					return StringLiteralExpressionIsSuspicious(literal);
+					return StringLiteralExpressionIsSuspicious(literal, isFirst);
 
 				case SyntaxKind.AddExpression:
 					var add = (BinaryExpressionSyntax)expression;
-					return ExpressionIsSuspicious(semanticModel, add.Left, cancellationToken) || ExpressionIsSuspicious(semanticModel, add.Right, cancellationToken);
+					return ExpressionIsSuspicious(semanticModel, add.Left, isFirst, cancellationToken) || ExpressionIsSuspicious(semanticModel, add.Right, isFirst: false, cancellationToken);
 
 				case SyntaxKind.InterpolatedStringExpression:
 					var interpolatedString = (InterpolatedStringExpressionSyntax)expression;
-					return InterpolatedStringIsSuspicious(interpolatedString);
+					return InterpolatedStringIsSuspicious(interpolatedString, isFirst);
 
 				case SyntaxKind.IdentifierName:
-					return IdentifierNameIsSuspicious(semanticModel, expression, cancellationToken);
+					return IdentifierNameIsSuspicious(semanticModel, expression, isFirst, cancellationToken);
 
 				default:
 					return false;
 			}
 		}
 
-		static bool StringLiteralExpressionIsSuspicious(LiteralExpressionSyntax syntax)
+		static bool StringLiteralExpressionIsSuspicious(LiteralExpressionSyntax syntax, bool isFirst)
 		{
 			var text = syntax.Token.ValueText;
-			return TextContainsPathSeparator(text);
+			return TextContainsPathSeparator(text, isFirst);
 		}
 
-		static bool InterpolatedStringIsSuspicious(InterpolatedStringExpressionSyntax syntax)
+		static bool InterpolatedStringIsSuspicious(InterpolatedStringExpressionSyntax syntax, bool isFirst)
 		{
-			foreach (var section in syntax.Contents)
+			var contents = syntax.Contents;
+			for (var i = 0; i < contents.Count; i++)
 			{
+				var section = contents[i];
 				if (!section.IsKind(SyntaxKind.InterpolatedStringText))
 				{
 					continue;
 				}
 
 				var text = (InterpolatedStringTextSyntax)section;
-				if (TextContainsPathSeparator(text.TextToken.ValueText))
+				if (TextContainsPathSeparator(text.TextToken.ValueText, isFirst && i == 0))
 				{
 					return true;
 				}
@@ -152,7 +174,7 @@ namespace WTG.Analyzers
 			return false;
 		}
 
-		static bool IdentifierNameIsSuspicious(SemanticModel semanticModel, ExpressionSyntax expression, CancellationToken cancellationToken)
+		static bool IdentifierNameIsSuspicious(SemanticModel semanticModel, ExpressionSyntax expression, bool isFirst, CancellationToken cancellationToken)
 		{
 			var constant = semanticModel.GetConstantValue(expression, cancellationToken);
 			if (!constant.HasValue || !(constant.Value is string value))
@@ -160,17 +182,51 @@ namespace WTG.Analyzers
 				return false;
 			}
 
-			return TextContainsPathSeparator(value);
+			return TextContainsPathSeparator(value, isFirst);
 		}
 
-		static bool TextContainsPathSeparator(string text)
+		static bool TextContainsPathSeparator(string text, bool isFirst)
 		{
+			if (isFirst && IsLikelyWellKnownPlatformSpecificPath(text))
+			{
+				return false;
+			}
+
 			for (var i = 0; i < text.Length; i++)
 			{
 				if (text[i] == '/' || text[i] == '\\')
 				{
 					return true;
 				}
+			}
+
+			return false;
+		}
+
+		static bool IsLikelyWellKnownPlatformSpecificPath(string text)
+		{
+			if (text.Length == 0)
+			{
+				// No text to examine
+				return false;
+			}
+
+			if (text[0] == '/')
+			{
+				// likely UNIX-style path (/blah/foo)
+				return true;
+			}
+
+			if (text.StartsWith(@"\\", StringComparison.Ordinal))
+			{
+				// likely Windows UNC path or extended path (\\server\share or \\?\ prefix)
+				return true;
+			}
+
+			if (text.Length > 1 && char.IsLetter(text[0]) && text[1] == ':')
+			{
+				// likely Windows local path with drive letter (X:\path)
+				return true;
 			}
 
 			return false;
