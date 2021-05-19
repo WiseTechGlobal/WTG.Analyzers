f758a172c5eb0c1addd42580eff6b6f9f5285f2d
Yaakov Smith <yaakov.smith@wisetechglobal.com>
2021-05-20 09:33:16 +1000

    simplify

--- WTG.Analyzers/Analyzers/FileSystemPaths/FileSystemPathsAnalyzer.cs
+++ WTG.Analyzers/Analyzers/FileSystemPaths/FileSystemPathsAnalyzer.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Immutable;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
