244d063ba524452d91557880d36eb88f93d27727
Yaakov Smith <yaakov.smith@wisetechglobal.com>
2021-05-19 17:24:11 +1000

    Add tests for explicit array init

---
+++ WTG.Analyzers.Test/TestData/FileSystemPathsAnalyzer/PathCombineArray/Result.cs
@@ -0,0 +1,25 @@
+using System.IO;
+
+class Foo
+{
+	const string Child = "grandchild";
+	const string GrandchildFolderName = "grandchild";
+
+	public static void Method()
+	{
+		DoThingWithFile(Path.Combine(new string[] { "parent", "child", "grandchild" }));
+		DoThingWithFile(Path.Combine(new string[] { "parent", "child", "grandchild" }));
+		DoThingWithFile(Path.Combine(new string[] { "parent", "child", GrandchildFolderName }));
+		DoThingWithFile(Path.Combine(new string[] { "parent", "child", GrandchildFolderName }));
+		DoThingWithFile(Path.Combine(new string[] { "parent", "child", GrandchildFolderName }));
+		DoThingWithFile(Path.Combine(new string[] { "parent", Child, GrandchildFolderName }));
+		DoThingWithFile(Path.Combine(new string[] { "parent", Child, GrandchildFolderName }));
+		DoThingWithFile(Path.Combine(new string[] { "parent", "child", "grandchild" + 123 }));
+		DoThingWithFile(Path.Combine(new string[] { "parent", 123 + "child", "grandchild" }));
+		DoThingWithFile(Path.Combine(new string[] { "parent", "child", $"prefixed{GrandchildFolderName}.001" }));
+	}
+
+	static void DoThingWithFile(string file)
+	{
+	}
+}
