103af922ab4d2e5231ca851d0a75474c73c2b553
brian-reichle <18721383+brian-reichle@users.noreply.github.com>
2022-03-02 17:44:51 +1100

    Normalize comment endings.

--- src/WTG.Analyzers.Test/TestData/VarAnalyzer/Locals/Source.cs
+++ src/WTG.Analyzers.Test/TestData/VarAnalyzer/Locals/Source.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 
 public class Bob
 {
 	public void Method(int value)
 	{
-		int unvarableLocal; // can't translate this so don't suggest it
+		int unvarableLocal; // can't translate this so don't suggest it.
 		int varableLocal = value;
-		object wrongType = value; // can't translate this, so don't suggest it
+		object wrongType = value; // can't translate this, so don't suggest it.
 		const int Value = 42; // can't use var on constants, so don't suggest it.
 		unvarableLocal = value;
 	}
