559ecaac6669976fe9384c450291cab0d6326c1a
Will Kennedy <will.kennedy@wisetechglobal.com>
2023-01-11 16:42:21 +1100

    Alphabetized WellKnownTypeNames.cs again

--- src/WTG.Analyzers.Utils/WellKnownTypeNames.cs
+++ src/WTG.Analyzers.Utils/WellKnownTypeNames.cs
@@ -5,11 +5,11 @@ namespace WTG.Analyzers.Utils
 #pragma warning disable CA1707 // Identifiers should not contain underscores
 #pragma warning disable SA1310 // Field names should not contain underscore
 		public const string Enumerable = "System.Linq.Enumerable";
+		public const string IEnumerable_T = "System.Collections.Generic.IEnumerable`1";
 		public const string IQueryable_T = "System.Linq.IQueryable`1";
+		public const string List_T = "System.Collections.Generic.List`1";
 		public const string Span = "System.Span`1";
 		public const string Task = "System.Threading.Tasks.Task";
-		public const string List_T = "System.Collections.Generic.List`1";
-		public const string IEnumerable_T = "System.Collections.Generic.IEnumerable`1";
 #pragma warning restore SA1310 // Field names should not contain underscore
 #pragma warning restore CA1707 // Identifiers should not contain underscores
 	}
