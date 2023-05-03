a34d9b3cf9967cec11dd99fc86aa81fa178274d3
Yaakov <yaakov.smith@wisetechglobal.com>
2023-05-03 15:54:31 +1000

    dont suppress that warning

--- src/WTG.Analyzers.Test/TestData/VisibilityAnalyzer/Partial/Source.cs
+++ src/WTG.Analyzers.Test/TestData/VisibilityAnalyzer/Partial/Source.cs
@@ -1,11 +1,9 @@
 partial class Foo
 {
 	private partial int Bar();
-	partial private int Baz();
 }
 
 partial class Foo
 {
 	private partial int Bar() { return default; }
-	partial private int Baz() { return default; }
 }
