6e9f2af481f59b6a9e8fc9945960b99c7f3ba537
brian-reichle <18721383+brian-reichle@users.noreply.github.com>
2023-05-12 09:41:14 +1000

    Split TopLevelInternal test.

---
+++ src/WTG.Analyzers.Test/TestData/VisibilityAnalyzer/TopLevelInternal/Compilation/Source.cs
@@ -0,0 +1,19 @@
+using System;
+
+internal class BobClass2
+{
+}
+
+internal struct BobStruct2
+{
+}
+
+internal interface BobInterface2
+{
+}
+
+internal enum BobEnum2
+{
+}
+
+internal delegate void BobDelegate2();
