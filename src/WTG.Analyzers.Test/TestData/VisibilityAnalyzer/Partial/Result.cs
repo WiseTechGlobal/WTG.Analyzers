0004a381f6978856375ec95380b80e492d512f0e
Munki Jeong <munki.jeong@wisetechglobal.com>
2024-07-02 11:18:08 +1000

    - add test cases

--- src/WTG.Analyzers.Test/TestData/VisibilityAnalyzer/Partial/Result.cs
+++ src/WTG.Analyzers.Test/TestData/VisibilityAnalyzer/Partial/Result.cs
@@ -3,11 +3,19 @@ namespace ns
 	partial class Foo
 	{
 		private partial int Bar();
+		public partial void FooBar();
+
+		partial void Qux();
+		private partial void Quux(out int value);
 	}
 
 	partial class Foo
 	{
 		private partial int Bar() { return default; }
+		public partial void FooBar() { }
+
+		partial void Qux() { }
+		private partial void Quux(out int value) { value = default; }
 	}
 
 	class Outer
